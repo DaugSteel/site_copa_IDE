b8c6990fb04e6f33eb3c2684752046ee93c4959e
Diego <daugsteel@gmail.com>
2022-12-11 01:39:55 -0300

    footer (incomplete)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,9 +3,24 @@ import styles from './style.module.css'
 export default function Footer()
 {
     return (
-        <div className={styles.footer}>
-            <img src="/Footer/cbf1.png" alt="CU">  
-            </img>
+        <div className={styles.container}>
+            <div className={styles.cbf}>
+                <img src="/cbf1.png" alt="Foto do logo da CBF"/>
+                <div>
+                    Confederação Brasileira de Futebol © x IDE © Todos os direitos reservados. 
+                    <br/>
+                    <br/>
+                    Termos de Uso | Politica de Privacidade | Política de Cookies
+                </div>
+                
+                <div>
+                    Realização:
+                    <br/>
+                    <img src="/IDE.png" alt="Foto do logo da IDE"/>    
+                </div>
+            </div>
+                
+
         </div>
     )
 }
