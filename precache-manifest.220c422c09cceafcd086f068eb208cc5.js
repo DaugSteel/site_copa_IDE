f5649373fc378c893026589e85e5ccda1ec1fe0c
Pillonetto <gabrielpillonetto@gmail.com>
2022-12-16 15:50:37 -0300

    Updates

---
+++ precache-manifest.220c422c09cceafcd086f068eb208cc5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "12e98559b63b50d28a62",
+    "url": "/site_copa_IDE/static/css/main.6e51c012.chunk.css"
+  },
+  {
+    "revision": "12e98559b63b50d28a62",
+    "url": "/site_copa_IDE/static/js/main.12e98559.chunk.js"
+  },
+  {
+    "revision": "0a3fad24fe19322116ed",
+    "url": "/site_copa_IDE/static/js/1.0a3fad24.chunk.js"
+  },
+  {
+    "revision": "1671bdcf4479f7a7cf43",
+    "url": "/site_copa_IDE/static/js/2.1671bdcf.chunk.js"
+  },
+  {
+    "revision": "e5168053c6fbad511b04",
+    "url": "/site_copa_IDE/static/js/runtime~main.e5168053.js"
+  },
+  {
+    "revision": "6dccb91d1c65fb1827052eff46094d3e",
+    "url": "/site_copa_IDE/static/media/cover.6dccb91d.png"
+  },
+  {
+    "revision": "6f29a4c17f5c919ec6fe241aa42bb195",
+    "url": "/site_copa_IDE/static/media/japao.6f29a4c1.png"
+  },
+  {
+    "revision": "8d8849080aae8616081c9aeb53c0f064",
+    "url": "/site_copa_IDE/static/media/coreia.8d884908.png"
+  },
+  {
+    "revision": "e35ee740515b2d311d2b201815f10590",
+    "url": "/site_copa_IDE/static/media/ghana.e35ee740.png"
+  },
+  {
+    "revision": "1f94258693a5123b36e36383713e93a9",
+    "url": "/site_copa_IDE/index.html"
+  }
+];
